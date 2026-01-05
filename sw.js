1dc311247c306913c84d1377d8dceb60509e440c
Manik Roy <pixnum@gmail.com>
2026-01-05 19:16:05 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,7 @@
-const CACHE_NAME = 'climix-cache-v1';
-const urlsToCache = ['./','./index.html','./manifest.json','./icon-512.png','./icon-192.png'];
-
-// Install SW
-self.addEventListener('install', e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+const CACHE="climix-v1";
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html"])));
 });
-
-// Activate SW
-self.addEventListener('activate', e=>{
- e.waitUntil(self.clients.claim());
-});
-
-// Fetch requests
-self.addEventListener('fetch', e=>{
- e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)));
+self.addEventListener("fetch",e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
