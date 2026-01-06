a3cd1a72a90b4cc9052cb54cb86b2895b6d069c7
Manik Roy <pixnum@gmail.com>
2026-01-06 17:43:18 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,12 @@
-const CACHE_NAME = "weathia-v1";
-const OFFLINE_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE="weathia-v1";
+const FILES=["./","index.html","manifest.json"];
 
-// Install
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_FILES))
-  );
-  self.skipWaiting();
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
 });
 
-// Activate (remove old caches)
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch strategy
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(event.request))
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
 });
