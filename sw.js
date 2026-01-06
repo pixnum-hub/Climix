49f97371c5c8f8a866fa21cad295bc7b5bd7f590
Manik Roy <pixnum@gmail.com>
2026-01-06 11:10:42 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,20 +4,25 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/icon-192.png',
-  '/icon-512.png',
-  // Add other JS/CSS if any
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(
-    keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
-  )));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
