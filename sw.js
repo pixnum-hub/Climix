aa2c5f0cacb817e920b314111056c471b134ac4a
Manik Roy <pixnum@gmail.com>
2026-01-06 14:44:27 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,12 @@
-const CACHE_NAME = 'climix-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-512.png'
-];
-
-// Install SW and cache essential files
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-// Activate and clean old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if(key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
+self.addEventListener("activate", e => {
+  self.clients.claim();
 });
 
-// Intercept fetch requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
-  );
-});
+/* IMPORTANT:
+   Do NOT intercept fetch.
+   Let network work normally.
+*/
